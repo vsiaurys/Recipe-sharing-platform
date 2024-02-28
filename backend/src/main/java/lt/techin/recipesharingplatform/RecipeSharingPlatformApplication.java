9def859354afcfae96c98ac891f4ccfba2df477d
unknown <siaurys@gmail.com>
2024-02-28 13:20:49 +0200

    Create Spring Boot project and add it to backend folder

---
+++ backend/src/main/java/lt/techin/recipesharingplatform/RecipeSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.recipesharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSharingPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lt/techin/recipesharingplatform/RecipeSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.recipesharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
