b08253240e0c6b904553bb5aff6978d81848636a
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-28 15:07:05 +0200

    Add header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,58 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <div>
+          <h1 className="text-light">Recipe Sharing Platform</h1>
+        </div>
+        <a
+          className="navbar-brand"
+          href="#"
+        ></a>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Recipes
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Categories
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
