ccffede1157d61b3d03325f787c579b630595dc2
Zygimantas <zyg.norkus@gmail.com>
2024-02-28 14:46:45 +0200

    Create Main.jsx component

---
+++ backend/src/assets/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+  return (
+    <>
+      <h3>Main</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        architecto, perspiciatis error nulla accusantium in.
+      </p>
+    </>
+  );
+}
