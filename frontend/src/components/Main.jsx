c113635bd890db578e25b4d8501f31097c40c238
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-28 16:22:26 +0200

    Modify navbar to be on the left side

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <p>
+        Welcome to the Recipe sharing platform. Here you can share and looks for
+        favorite recipes
+      </p>
+    </>
+  );
+}
