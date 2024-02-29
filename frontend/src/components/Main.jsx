2d270ff2adf902e8c4d5291bbbe72f871ff8713c
Zygimantas <zyg.norkus@gmail.com>
2024-02-29 10:39:06 +0200

    Correct className

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,11 +1,13 @@
 export default function Main() {
   return (
-    <div class="container">
-      <h1>Welcome!</h1>
-      <p>
-        Welcome to the Recipe sharing platform. Here you can share and looks for
-        favorite recipes
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <h1>Welcome!!!</h1>
+        <p>
+          Welcome to the Recipe sharing platform. Here you can share and looks
+          for favorite recipes
+        </p>
+      </div>
+    </>
   );
 }
