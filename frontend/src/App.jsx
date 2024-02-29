8a54d8207653df579d67d805566a4233126e0ab1
unknown <siaurys@gmail.com>
2024-02-29 13:52:50 +0200

    Update Footer (menu in 1 row for mobiles)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Main />
-      <Footer />;
+      <Footer />
     </>
   );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,24 +8,24 @@ export default function Footer() {
 
   return (
     <div className="fixed-bottom bg-dark">
-      <div className="container text-start p-3 text-white w-100 d-flex justify-content-between">
-        <div>
-          <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
-          <div>Contact email:</div>
-          <div>
-            <a href="mailto:vardas.pavarde@gmail.com">
-              vardas.pavarde@gmail.com
-            </a>
+      <div className="container text-start p-3 text-white w-100 ">
+        <div className="row">
+          <div className="col-sm-12 col-md-8 col-lg-9">
+            <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
+            <p className="text-secondary">Contact email:</p>
+            <p>
+              <a href="mailto:vardas.pavarde@gmail.com">
+                vardas.pavarde@gmail.com
+              </a>
+            </p>
           </div>
-        </div>
-        <div>
-          <p>
-            <div className="text-secondary">Our address: </div>Trinapolio g. 2,
-            Vilnius
-          </p>
-          <p>
-            <div className="text-secondary">Call us: </div>+370 5 269 7455
-          </p>
+
+          <div className="col-sm-12 col-md-4 col-lg-3">
+            <p className="text-secondary">Our address: </p>
+            <p>Trinapolio g. 2, Vilnius</p>
+            <p className="text-secondary">Call us: </p>
+            <p>+370 5 269 7455</p>
+          </div>
         </div>
       </div>
     </div>
