12079de76693f07091139250aef1c9cfaf5cfdff
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-29 11:12:47 +0200

    Add hamburger button to navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,44 +1,60 @@
+import { useState } from "react";
+
 function Header() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <div className="bg-dark">
       <nav className="container navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container d-flex">
+        <div className="container d-flex justify-content-between">
           <p className="my-auto navbar-brand">Recipe Sharing Platform</p>
-
-          <div
-            className="justify-content-end"
-            id="navbarNav"
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={toggleNavbar}
           >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Login
-                </a>
-              </li>
-            </ul>
-          </div>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+
+        <div
+          className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Register
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                Login
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
   );
 }
+
 export default Header;
