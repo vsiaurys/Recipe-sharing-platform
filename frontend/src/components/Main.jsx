92445b3a97e1bb47ace872199ec5245d6075af47
Zygimantas <zyg.norkus@gmail.com>
2024-02-29 10:59:41 +0200

    Correct Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,7 +2,7 @@ export default function Main() {
   return (
     <>
       <div className="container">
-        <h1>Welcome!!!</h1>
+        <h1>Welcome</h1>
         <p>
           Welcome to the Recipe sharing platform. Here you can share and looks
           for favorite recipes
