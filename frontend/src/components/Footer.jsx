58972845b78183dcd39a301f57db8bb0283b8c0d
unknown <siaurys@gmail.com>
2024-02-29 15:16:03 +0200

    Update colors in Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import "./Footer.css";
+
 export default function Footer() {
   const getCurrentYear = () => {
     const currentDate = new Date();
@@ -7,23 +9,26 @@ export default function Footer() {
   };
 
   return (
-    <div className="fixed-bottom bg-dark">
-      <div className="container text-start p-3 text-white w-100 ">
+    <div className="fixed-bottom footer">
+      <div className="container text-start p-3 w-100 ">
         <div className="row">
           <div className="col-sm-12 col-md-8 col-lg-9">
             <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
-            <p className="text-secondary">Contact email:</p>
+            <p>Contact email:</p>
             <p>
-              <a href="mailto:vardas.pavarde@gmail.com">
+              <a
+                className="text-decoration-none contact-email"
+                href="mailto:vardas.pavarde@gmail.com"
+              >
                 vardas.pavarde@gmail.com
               </a>
             </p>
           </div>
 
           <div className="col-sm-12 col-md-4 col-lg-3">
-            <p className="text-secondary">Our address: </p>
+            <p>Our address: </p>
             <p>Trinapolio g. 2, Vilnius</p>
-            <p className="text-secondary">Call us: </p>
+            <p>Call us: </p>
             <p>+370 5 269 7455</p>
           </div>
         </div>
