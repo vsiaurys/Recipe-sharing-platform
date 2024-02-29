750f8ae13e9b40fbae4ac0cd46165f66774f6d50
unknown <siaurys@gmail.com>
2024-02-29 11:32:51 +0200

    Add contact iformation to Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 
 function App() {
-  //const [count, setCount] = useState(0);
-
   return <Footer />;
 }
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,12 +8,25 @@ export default function Footer() {
 
   return (
     <div className="fixed-bottom bg-dark">
-      <div className="container text-start p-3 text-white w-100">
-        <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
-        <p>
-          Contact email:&nbsp;
-          <a href="mailto:vardas.pavarde@gmail.com">vardas.pavarde@gmail.com</a>
-        </p>
+      <div className="container text-start p-3 text-white w-100 d-flex justify-content-between">
+        <div>
+          <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
+          <p>
+            Contact email:&nbsp;
+            <a href="mailto:vardas.pavarde@gmail.com">
+              vardas.pavarde@gmail.com
+            </a>
+          </p>
+        </div>
+        <div>
+          <p>
+            <span className="text-secondary">Our address: </span>Trinapolio g.
+            2, Vilnius
+          </p>
+          <p>
+            <span className="text-secondary">Call us: </span>+370 5 269 7455
+          </p>
+        </div>
       </div>
     </div>
   );
