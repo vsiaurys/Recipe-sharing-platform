468018e1459d399a0c250b38bffe593085245473
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-29 10:38:21 +0200

    Modify header so sides would be black as container, moved bootsrap import to main.jsx, Logo is centerd

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,46 +1,44 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
 function Header() {
   return (
-    <nav className="container navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <p className="navbar-brand justify-content-end">
-          Recipe Sharing Platform
-        </p>
+    <div className="bg-dark">
+      <nav className="container navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container d-flex">
+          <p className="my-auto navbar-brand">Recipe Sharing Platform</p>
 
-        <div
-          className="justify-content-end"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#"
-              >
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#"
-              >
-                Register
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#"
-              >
-                Login
-              </a>
-            </li>
-          </ul>
+          <div
+            className="justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                >
+                  Register
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                >
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 export default Header;
