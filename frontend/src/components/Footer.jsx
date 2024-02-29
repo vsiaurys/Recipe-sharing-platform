b04b05a722833e751af655b51fce12a6015227e2
unknown <siaurys@gmail.com>
2024-02-29 12:36:11 +0200

    Update Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,12 +11,12 @@ export default function Footer() {
       <div className="container text-start p-3 text-white w-100 d-flex justify-content-between">
         <div>
           <p>&#169; {getCurrentYear()} RECIPE SHARING PLATFORM</p>
-          <p>
-            Contact email:&nbsp;
+          <div>Contact email:</div>
+          <div>
             <a href="mailto:vardas.pavarde@gmail.com">
               vardas.pavarde@gmail.com
             </a>
-          </p>
+          </div>
         </div>
         <div>
           <p>
