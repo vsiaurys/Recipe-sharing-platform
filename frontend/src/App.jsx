6390174f43833773ec6f08702fc940e7cdb6c658
Zygimantas <zyg.norkus@gmail.com>
2024-03-01 10:49:39 +0200

    Add Register.jsx and Routes imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,26 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import Register from "./components/Register";
+import { Route, Routes, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+      <p>
+        <Routes>
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+        </Routes>
+      </p>
+    </>
   );
 }
 
