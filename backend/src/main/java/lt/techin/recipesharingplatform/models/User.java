e851b3c8d4482a266a59d05e2e0c37cba10fb321
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-01 11:35:01 +0200

    Add methods for UserDetails interface implementation

--- backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
@@ -5,10 +5,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -117,6 +122,11 @@ public class User {
         return password;
     }
 
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
     public String getDisplayName() {
         return displayName;
     }
@@ -135,5 +145,30 @@ public class User {
 
     public String getRole() {
         return role;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> this.getRole());
     }
 }
