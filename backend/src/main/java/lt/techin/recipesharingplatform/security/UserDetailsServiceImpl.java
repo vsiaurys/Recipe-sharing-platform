eb8131562a72faa654e925567b90f037f5787739
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-01 14:20:07 +0200

    Add UserServiceDetailsImp and Create UserRepository Class

---
+++ backend/src/main/java/lt/techin/recipesharingplatform/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package lt.techin.recipesharingplatform.security;
+
+import lt.techin.recipesharingplatform.models.User;
+import lt.techin.recipesharingplatform.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository
+                .findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return user;
+    }
+}
