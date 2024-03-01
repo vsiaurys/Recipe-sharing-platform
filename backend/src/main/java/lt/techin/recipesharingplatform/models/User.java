61962924bf45c6a68fb43fb43821610954eb8949
unknown <siaurys@gmail.com>
2024-03-01 13:06:39 +0200

    Add validation to User class

--- backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
@@ -14,6 +14,24 @@ import java.util.List;
 @Entity
 @Table(name = "Users")
 public class User implements UserDetails {
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])" + // At least one digit
+            "(?=.*[a-z])"
+            + // At least one lowercase letter
+            "(?=.*[A-Z])"
+            + // At least one uppercase letter
+            "(?=.*[@#$%^&+=])"
+            + // At least one special character
+            "(?=\\S+$)"
+            + // No whitespace allowed
+            ".{6,20}$"; // From 6 to 20 characters long
+
+    private static final String NAME_REGEX = "^(?!\\w*(\\w)\\1{5})"
+            + // Negative lookahead to ensure no character is repeated more than 5 times consecutively
+            "(?=\\p{Lu}\\p{L}+$)"
+            + // Positive lookahead to ensure the string starts with an uppercase letter followed by one or more
+            // letters
+            "[A-Za-z]+$"; // Character class to match any uppercase or lowercase letter
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -22,41 +40,39 @@ public class User implements UserDetails {
     @NotEmpty(message = "Email cannot be empty")
     private String email;
 
-    // @Password ////////////////////////
+    @Pattern(
+            regexp = PASSWORD_REGEX,
+            message =
+                    "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and no whitespace.")
+    @Size(min = 6, max = 20, message = "Password must be in range between 6 and 20 characters")
     @NotEmpty(message = "Password cannot be empty")
     private String password;
-    //   Must include at least 1 uppercase, at least 1 number, at least 1 special symbol. Not
-    // least 6 characters, not
-    // more than 20. Cannot have empty spaces.
-    //
 
     @Column(unique = true)
     @NotEmpty(message = "Display name cannot be empty")
     @Size(min = 3, max = 15, message = "Display name must be in range between 3 and 15 characters")
     @Pattern(regexp = "^[a-zA-Z]{3,15}$", message = "Display name may contain only lowercase or uppercase letters")
     private String displayName;
-
-    //    //
-    //    //                                        Cannot have offensive words.
+    // Cannot have offensive words.
 
     @NotEmpty(message = "First name cannot be empty")
+    @Pattern(
+            regexp = NAME_REGEX,
+            message =
+                    "First name must start out of a letter in uppercase and may contain only lowercase or uppercase letters (minimum 2 characters)")
     private String firstName;
-    //                                           Cannot have symbols, spaces. First letter must be uppercase. At least 2
-    // characters. Same symbol cannot repeat
-    // more than 5 times.
-    //
 
     @NotEmpty(message = "Last name cannot be empty")
+    @Pattern(
+            regexp = NAME_REGEX,
+            message =
+                    "Last name must start out of a letter in uppercase and may contain only lowercase or uppercase letters (minimum 2 characters)")
     private String lastName;
-    //                                                    Cannot have symbols, spaces. First letter must be uppercase.
-    // At
-    // least 2 characters. Same symbol cannot repeat
-    // more than 5 times.
-    //
+
     @NotEmpty(message = "Gender cannot be empty")
     private String gender;
-    //                                              Must be words Male, Female, or Other
-    //
+    // Must be words Male, Female, or Other
+
     @NotEmpty(message = "Role cannot be empty")
     @Pattern(
             regexp = "^ROLE_[A-Z]{1,25}$",
