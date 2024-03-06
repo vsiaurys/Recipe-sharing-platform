daa45bd4efcb00ec7317773b609b878ae8fd4781
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-06 13:41:44 +0200

    Create LoginSuccessful Componemt

---
+++ frontend/src/components/LoginSuccessful.jsx
@@ -0,0 +1,13 @@
+function LoginSuccessful() {
+  return (
+    <div className="container mx-auto mt-5">
+      <div
+        className="alert alert-success"
+        role="alert"
+      >
+        Congrats! Your login was successful!
+      </div>
+    </div>
+  );
+}
+export default LoginSuccessful;
