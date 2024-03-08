f00a761f0dc37af82fe6eaaf45bdc6c939ae39ad
Zygimantas <zyg.norkus@gmail.com>
2024-03-08 16:29:18 +0200

    Add @CrossOrigin annotation

--- backend/src/main/java/lt/techin/recipesharingplatform/controllers/UserController.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/controllers/UserController.java
@@ -6,6 +6,7 @@ import lt.techin.recipesharingplatform.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
