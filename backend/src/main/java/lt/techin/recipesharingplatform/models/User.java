d420a49d396939b73da37e881968423f7dea2b99
Zygimantas <zyg.norkus@gmail.com>
2024-03-08 16:31:38 +0200

    Correct special symbols argument

--- backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/models/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
         @Pattern(regexp = ".*\\d.*", message = "Password must contain at least one digit"),
         @Pattern(regexp = ".*[a-z].*", message = "Password must contain at least one lowercase letter"),
         @Pattern(regexp = ".*[A-Z].*", message = "Password must contain at least one uppercase letter"),
-        @Pattern(regexp = ".*[@#$%^&+=].*", message = "Password must contain at least one special character"),
+        @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "Password must contain at least one special character"),
         @Pattern(regexp = "[^\\s]+", message = "No whitespace allowed")
     })
     private String password;
