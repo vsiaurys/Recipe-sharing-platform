81afbdf2d6a5701b2e234634097ebc73d0cbf105
vsiaurys <siaurys@gmail.com>
2024-03-12 16:22:15 +0200

    Update test createUser_whenBadEmail_thenReturnBadRequest

--- backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
+++ backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
@@ -251,6 +251,10 @@ public class UserControllerTest {
                 //  then
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.email").value("Please provide a valid email address"));
+
+        verify(this.userService, times(0)).saveUser(any(User.class));
+        verify(this.userService, times(0)).existsUserByEmail("");
+        verify(this.userService, times(0)).existsUserByDisplayName("");
     }
 
     @Test
