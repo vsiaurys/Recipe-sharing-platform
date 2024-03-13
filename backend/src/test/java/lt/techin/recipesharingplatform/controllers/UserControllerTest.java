4b796c7a26db6b335514d77debe554c93c85bd62
vsiaurys <siaurys@gmail.com>
2024-03-13 10:01:08 +0200

    Create test createUser_whenFirstNameContainsDigits_thenReturnBadRequest

--- backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
+++ backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
@@ -704,4 +704,34 @@ public class UserControllerTest {
         verify(this.userService, times(0)).existsUserByEmail("email@email.com");
         verify(this.userService, times(0)).existsUserByDisplayName("Display1");
     }
+
+    @Test
+    void createUser_whenFirstNameContainsDigits_thenReturnBadRequest() throws Exception {
+        //  given
+
+        //  when
+        mockMvc.perform(
+                        post("/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(
+                                        """
+                                                                         {
+                                                                             "displayName": "Display1",
+                                                                             "email": "email@email.com",
+                                                                             "password": "Password=1",
+                                                                             "firstName": "Name123",
+                                                                             "lastName": "Pavarde",
+                                                                             "gender": "Female"
+                                                                         }
+                                                                         """))
+
+                //  then
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.firstName").value("First name may contain only letters"));
+
+        verify(this.userService, times(0)).saveUser(any(User.class));
+        verify(this.userService, times(0)).existsUserByEmail("email@email.com");
+        verify(this.userService, times(0)).existsUserByDisplayName("Display1");
+    }
 }
