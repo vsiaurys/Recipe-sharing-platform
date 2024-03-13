b51b5b02fa5b4a757912811084fd5a1aa275fef6
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-13 11:28:35 +0200

    Modify container to have margin bottom

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -49,7 +49,7 @@ function Login() {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-4 mb-4">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card">
