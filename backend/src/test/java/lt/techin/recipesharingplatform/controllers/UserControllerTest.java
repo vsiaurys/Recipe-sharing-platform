ce378e8aa3919bfc14cb9e26833c8359eb4360ad
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-14 13:15:59 +0200

    Delete tests (done by other person who will update them with better versions)

--- backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
+++ backend/src/test/java/lt/techin/recipesharingplatform/controllers/UserControllerTest.java
@@ -9,14 +9,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -34,76 +31,6 @@ public class UserControllerTest {
 
     @MockBean
     private PasswordEncoder passwordEncoder;
-
-    @Test
-    @WithMockUser(roles = {"USER"})
-    void createUser_whenCreateUser_thenReturnIt() throws Exception {
-        //  given
-        User user = new User(
-                "abcdefghi.klmno49@efghijk.com",
-                "Password123&=",
-                "Smauglys87",
-                "Vardas",
-                "Pavarde",
-                "Male",
-                "ROLE_USER");
-        given(this.userService.saveUser(any(User.class))).willReturn(user);
-
-        // ObjectMapper om = new ObjectMapper();
-        // om.findAndRegisterModules();
-
-        //  when
-        mockMvc.perform(
-                        post("/register")
-                        //       .contentType(MediaType.APPLICATION_JSON)
-                        //       .accept(MediaType.APPLICATION_JSON)
-                        // .content(om.writeValueAsString(user)))
-                        )
-                //  then
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.email").value("abcdefghi.klmno49@efghijk.com"))
-                // .andExpect(jsonPath("$.password").value("Password123&="))
-                .andExpect(jsonPath("$.displayName").value("Smauglys87"))
-                .andExpect(jsonPath("$.firstName").value("Vardas"))
-                .andExpect(jsonPath("$.lastName").value("Pavarde"))
-                .andExpect(jsonPath("$.gender").value("Male"))
-                .andExpect(jsonPath("$.role").value("ROLE_USER"));
-
-        verify(this.userService).saveUser(any(User.class));
-    }
-
-    // public PasswordEncoder passwordEncoder() {
-    //        return new BCryptPasswordEncoder();
-
-    @Test
-    @WithMockUser
-    void createUser_whenNotAllowed_returnBadRequest() throws Exception {
-        //  given
-        User user = new User(
-                "abcdefghi.klmno49@efghijk.com",
-                "Password123&=",
-                "Smauglys87",
-                "Vardas",
-                "Pavarde",
-                "Male",
-                "ROLE_USER");
-        given(this.userService.saveUser(any(User.class))).willReturn(user);
-
-        //        ObjectMapper om = new ObjectMapper();
-        //        om.findAndRegisterModules();
-
-        //  when
-        mockMvc.perform(
-                        post("/register")
-                        //                        .contentType(MediaType.APPLICATION_JSON)
-                        //                        .accept(MediaType.APPLICATION_JSON)
-                        //                        .content(om.writeValueAsString(user)))
-                        )
-                //  then
-                .andExpect(status().isBadRequest());
-
-        verify(this.userService, times(0)).saveUser(any(User.class));
-    }
 
     @Test
     public void login_withIncorrectEmail_returnUnauthorized() throws Exception {
