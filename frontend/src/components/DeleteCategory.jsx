c2c906cfb791c59b458e3256360c2fe4733b242e
vsiaurys <siaurys@gmail.com>
2024-03-15 14:22:34 +0200

    Create DeleteCategory.jsx component

---
+++ frontend/src/components/DeleteCategory.jsx
@@ -0,0 +1,9 @@
+export default function DeleteCategory() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center">Delete Category</h1>
+      </div>
+    </>
+  );
+}
