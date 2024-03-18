c0c9a5d408155f785581142d7766cd1bc0f48484
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-18 13:37:05 +0200

    Add modal for logout without react-bootstrap

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import "./Header.css";
 import { Link, useNavigate } from "react-router-dom";
-import ModalLogout from "./ModalLogout";
 
 function Header({ isLoggedIn, setIsLoggedIn }) {
   const [collapsed, setCollapsed] = useState(true);
   const [showModal, setShowModal] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("role");
     setShowModal(true);
+    setShowOverlay(true);
+    setTimeout(() => {
+      setShowModal(false);
+      setShowOverlay(false);
+    }, 3000);
     navigate("/");
   };
 
@@ -80,7 +85,35 @@ function Header({ isLoggedIn, setIsLoggedIn }) {
           </div>
         </div>
       </nav>
-      {showModal && <ModalLogout setShowModal={setShowModal} />}
+      {showOverlay && <div className="overlay"></div>}
+      <div
+        className={`modal fade ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none" }}
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div
+          className="modal-dialog modal-dialog-centered"
+          role="document"
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2
+                className="modal-title text-success"
+                id="exampleModalCenterTitle"
+              >
+                Logout Successful!
+              </h2>
+            </div>
+            <div className="modal-body">
+              You have been successfully logged out. Thank you for using our
+              platform.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
