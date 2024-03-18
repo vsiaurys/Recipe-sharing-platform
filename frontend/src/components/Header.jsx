b2f3b09a4cd447c39bfd5df485c3c1d7ec00f7d1
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-18 13:50:43 +0200

    Add bootstrap styling for overlay

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -85,7 +85,12 @@ function Header({ isLoggedIn, setIsLoggedIn }) {
           </div>
         </div>
       </nav>
-      {showOverlay && <div className="overlay"></div>}
+      {showOverlay && (
+        <div
+          className="position-fixed top-0 left-0 w-100 h-100 bg-dark opacity-75"
+          style={{ zIndex: 999 }}
+        ></div>
+      )}
       <div
         className={`modal fade ${showModal ? "show" : ""}`}
         style={{ display: showModal ? "block" : "none" }}
