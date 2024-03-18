160bf5a1268fdd799761b2cd3d9329db87d5281c
Zygimantas <zyg.norkus@gmail.com>
2024-03-18 11:33:04 +0200

    Create Category.java class

---
+++ backend/src/main/java/lt/techin/recipesharingplatform/models/Category.java
@@ -0,0 +1,40 @@
+package lt.techin.recipesharingplatform.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Size(min = 4, max = 20, message = "Name must be from 4 to 20 characters")
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Name must contain only letters and spaces")
+    @Column(unique = true)
+    private String name;
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
