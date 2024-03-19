c35247ae0374fc45fee62e3815f04fd4f339c7c9
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-19 13:36:22 +0200

    Create forceRender state hook to re render main page each time you logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
 import "./App.css";
-import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import { useState } from "react";
 
 import { Route, Routes, NavLink } from "react-router-dom";
-import LoginSuccessful from "./components/LoginSuccessful";
 
 function App() {
+  const [forceRender, setForceRender] = useState(false);
   const checkRole = () => {
     const role = localStorage.getItem("role");
     return role !== null && role !== "";
   };
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Header checkRole={checkRole} />
+      <Header
+        checkRole={checkRole}
+        forceRender={forceRender}
+        setForceRender={setForceRender}
+      />
       <Routes>
         <Route
           path="/"
-          element={<Main />}
+          element={
+            <Main
+              checkRole={checkRole}
+              forceRender={forceRender}
+            />
+          }
         />
         <Route
           path="/login"
@@ -29,10 +38,6 @@ function App() {
         <Route
           path="/register"
           element={<Register />}
-        />
-        <Route
-          path="/login-successful"
-          element={<LoginSuccessful />}
         />
       </Routes>
       <footer className="mt-auto footer">
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,13 +1,35 @@
-export default function Main() {
+import { useEffect } from "react";
+
+export default function Main({ checkRole, forceRender }) {
+  const role = localStorage.getItem("role");
+  const email = localStorage.getItem("email");
+  const displayName = localStorage.getItem("displayName");
+  useEffect(() => {}, [forceRender]);
   return (
     <>
-      <div className="container">
-        <h1>Welcome</h1>
-        <p>
-          Welcome to the Recipe sharing platform. Here you can share and look
-          for favorite recipes.
-        </p>
-      </div>
+      {checkRole() ? (
+        <div className="container mx-auto mt-5">
+          <div
+            className="alert alert-success"
+            role="alert"
+          >
+            Congrats! Your login was successful!
+          </div>
+          <div>
+            <h2>Welcome {displayName}!</h2>
+          </div>
+          <div>Your email is: {email}</div>
+          <div>Your role is: {role}</div>
+        </div>
+      ) : (
+        <div className="container">
+          <h1>Welcome</h1>
+          <p>
+            Welcome to the Recipe sharing platform. Here you can share and look
+            for favorite recipes.
+          </p>
+        </div>
+      )}
     </>
   );
 }
