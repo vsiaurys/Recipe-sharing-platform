c09cfeefb82a1736886df9b3c4870a0d23a56a66
Zygimantas <zyg.norkus@gmail.com>
2024-03-21 15:04:19 +0200

    Create testSaveCategoryAsAdmin method test

---
+++ backend/src/test/java/lt/techin/recipesharingplatform/controllers/CategoryControllerTest.java
@@ -0,0 +1,47 @@
+package lt.techin.recipesharingplatform.controllers;
+
+import lt.techin.recipesharingplatform.models.Category;
+import lt.techin.recipesharingplatform.security.SecurityConfig;
+import lt.techin.recipesharingplatform.services.CategoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(controllers = CategoryController.class)
+public class CategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void testSaveCategoryAsAdmin() throws Exception {
+        Category category = new Category("Test Category");
+        given(categoryService.saveCategory(any(Category.class))).willReturn(category);
+
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"TestCategory\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Test Category"))
+                .andExpect(jsonPath("$.id").exists());
+
+        verify(this.categoryService, times(1)).saveCategory(any(Category.class));
+    }
+}
