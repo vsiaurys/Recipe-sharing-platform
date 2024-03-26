dcfe7361e87a5edf32910469d577a61463eed3df
vsiaurys <siaurys@gmail.com>
2024-03-26 13:36:56 +0200

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -40,7 +40,6 @@ function Login({ checkRole }) {
         localStorage.setItem("displayName", responseData.displayName);
         localStorage.setItem("role", responseData.role);
         localStorage.setItem("email", responseData.email);
-
         localStorage.setItem("firstName", responseData.firstName);
         localStorage.setItem("lastName", responseData.lastName);
         localStorage.setItem("gender", responseData.gender);
