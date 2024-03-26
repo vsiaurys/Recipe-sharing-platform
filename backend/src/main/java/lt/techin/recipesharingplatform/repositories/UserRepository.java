4ff5896817a152f92683f7cc6a408c87f31b5d0c
vitalijmirnyj <goforitasvm@gmail.com>
2024-03-26 10:16:12 +0200

    Create findUserById method method

--- backend/src/main/java/lt/techin/recipesharingplatform/repositories/UserRepository.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/repositories/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
 
+    Optional<User> findUserById(long id);
+
     boolean existsByDisplayName(String displayName);
 
     boolean existsByEmail(String email);
