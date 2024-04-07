134acfa641bc8146aa4ae23d0015ff632fbbbf3c
vsiaurys <siaurys@gmail.com>
2024-04-07 13:34:27 +0300

    Create findCategoryById method to CategoryRepository

--- backend/src/main/java/lt/techin/recipesharingplatform/repositories/CategoryRepository.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package lt.techin.recipesharingplatform.repositories;
 import lt.techin.recipesharingplatform.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
 
     boolean existsById(long id);
+
+    Optional<Category> findCategoryById(long id);
 }
