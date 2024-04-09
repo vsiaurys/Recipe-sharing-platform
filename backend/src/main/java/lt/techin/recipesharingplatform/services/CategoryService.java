9a64c1cd308e08388dbf7f52b9067362f8d8f255
vsiaurys <siaurys@gmail.com>
2024-04-09 11:03:38 +0300

    Add method findCategoryByName to CategoryService

--- backend/src/main/java/lt/techin/recipesharingplatform/services/CategoryService.java
+++ backend/src/main/java/lt/techin/recipesharingplatform/services/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -38,5 +39,9 @@ public class CategoryService {
 
     public Category findCategoryById(long id) {
         return categoryRepository.findCategoryById(id);
+    }
+
+    public Optional<Category> findCategoryByName(String name) {
+        return categoryRepository.findCategoryByName(name);
     }
 }
